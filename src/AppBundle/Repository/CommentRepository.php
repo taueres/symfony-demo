164ec19f635885013acf09d4944bdd90c7e66053
Sergio Santoro <santoro.srg@gmail.com>
2015-10-10 18:14:34 +0200

    getCommentsByEmailAddress

--- src/AppBundle/Repository/CommentRepository.php
+++ src/AppBundle/Repository/CommentRepository.php
@@ -2,9 +2,17 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\Comment;
 use Doctrine\ORM\EntityRepository;
 
 class CommentRepository extends EntityRepository
 {
-
+    /**
+     * @param string $email
+     * @return array|Comment[]
+     */
+    public function getCommentsByEmailAddress($email)
+    {
+        return $this->findBy(['authorEmail' => $email]);
+    }
 }
